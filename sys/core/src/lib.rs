2a1fd83abea89ca47d18271f41df6972f7b491e7
richardwesthaver <richard.westhaver@gmail.com>
2021-02-05 20:24:43 -0500

    removed old prometheus network components in utils/prometheus

--- sys/core/src/lib.rs
+++ sys/core/src/lib.rs
@@ -1,6 +1,3 @@
-//! A collection of structures and functions useful across the entire lib.
-//#![feature(maybe_uninit_ref)]
-
 pub mod proto;
 pub use self::proto::VarInt;
 mod circular_buffer;
--- sys/core/src/tests.rs
+++ sys/core/src/tests.rs
@@ -1,1 +1,8 @@
+use std::{ops::RangeFrom, sync::Mutex};
 
+use lazy_static::lazy_static;
+
+lazy_static! {
+  pub static ref SERVER_PORTS: Mutex<RangeFrom<u16>> = Mutex::new(8888..);
+  pub static ref CLIENT_PORTS: Mutex<RangeFrom<u16>> = Mutex::new(32328..);
+}
