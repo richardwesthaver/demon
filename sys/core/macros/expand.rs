a89691a38344ee6c807622ea00f56fb0136d43c4
richardwesthaver <richard.westhaver@gmail.com>
2021-02-07 04:34:18 -0500

    added core init macros!

---
+++ sys/core/macros/expand.rs
@@ -0,0 +1,82 @@
+use proc_macro2::TokenStream;
+
+use quote::quote;
+use syn::punctuated::Punctuated;
+use syn::{parse_quote, Error, ItemFn, Result};
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum Mode {
+		Main,
+		Test,
+//    CompatTest,
+}
+
+pub fn expand(mode: Mode, mut function: ItemFn) -> Result<TokenStream> {
+		if function.sig.inputs.len() > 1 {
+				return Err(Error::new_spanned(
+						function.sig,
+						"expected one argument of type dmeon_core::DemonInit",
+				));
+		}
+
+		if mode == Mode::Main && function.sig.ident != "main" {
+				return Err(Error::new_spanned(
+						function.sig,
+						"#[demon_core::main] must be used on the main function!",
+				));
+		}
+
+		let guard = match mode {
+				Mode::Main => Some(quote! {
+						if module_path!().contains("::") {
+								panic!("demon_core::init must be performed in the crate root of the main function");
+						}
+				}),
+				Mode::Test => None, // | Mode::CompatTest
+		};
+
+		let assignment = function.sig.inputs.first().map(|arg| quote!(let #arg =));
+		function.sig.inputs = Punctuated::new();
+
+		let block = function.block;
+		
+    let body = match (function.sig.asyncness.is_some(), mode) {
+				(true, Mode::Test) => quote! {
+						tokio::runtime::Builder::new()
+								.basic_scheduler()
+								.enable_all()
+								.build()
+								.unwrap()
+								.block_on(async #block)
+				},
+				(true, Mode::Main) => quote! {
+						tokio::runtime::Builder::new()
+								.threaded_scheduler()
+								.enable_all()
+								.build()
+								.unwrap()
+								.block_on(async #block)
+				},
+        (false, _) => {
+            let stmts = block.stmts;
+            quote! { #(#stmts)* }
+        }
+		};
+
+		function.block = parse_quote!({
+				#guard
+				#assignment unsafe {
+						demon_core::r#impl::perform_init()
+				};
+				let destroy_guard = unsafe { demon_core::r#impl::DestroyGuard::new() };
+				#body
+		});
+
+		function.sig.asyncness = None;
+
+		if mode == Mode::Test {
+				function.attrs.push(parse_quote!(#[test]));
+		}
+
+		Ok(quote!(#function))
+}
---
+++ sys/core/macros/lib.rs
@@ -0,0 +1,44 @@
+mod expand;
+use expand::{expand, Mode};
+use proc_macro::TokenStream;
+use syn::parse_macro_input;
+use syn::parse::Nothing;
+/// Expands from:
+///
+/// #[demon_core::main]
+/// fn main(dm: DemonInit) {
+///  ...
+/// }
+///
+/// to:
+///
+/// fn main() {
+///   let dm: DemonInit = demon_core::r#impl::perform_init();
+///   ...
+/// }
+///
+/// if async, also adds a #[tokio: main] attribute
+#[proc_macro_attribute]
+pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
+		parse_macro_input!(args as Nothing);
+		expand(Mode::Main, parse_macro_input!(input))
+				.unwrap_or_else(|err| err.to_compile_error())
+				.into()
+}
+
+/// provides same functionality as 'main' but for #[test] and #[tokio::test]
+#[proc_macro_attribute]
+pub fn test(args: TokenStream, input: TokenStream) -> TokenStream {
+    parse_macro_input!(args as Nothing);
+    expand(Mode::Test, parse_macro_input!(input))
+        .unwrap_or_else(|err| err.to_compile_error())
+        .into()
+}
+
+// #[proc_macro_attribute]
+// pub fn compat_test(args: TokenStream, input: TokenStream) -> TokenStream {
+//     parse_macro_input!(args as Nothing);
+//     expand(Mode::CompatTest, parse_macro_input!(input))
+//         .unwrap_or_else(|err| err.to_compile_error())
+//         .into()
+// }
