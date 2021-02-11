a121047f163ab99e62b773b2fa79d6a1923df307
richardwesthaver <richard.westhaver@gmail.com>
2021-02-11 06:59:21 -0500

    hopefully back to cross-platform support now

--- utils/tools/src/os/mod.rs
+++ utils/tools/src/os/mod.rs
@@ -1,6 +1,15 @@
 #[cfg(target_os = "linux")]
 pub(self) mod linux;
 
+#[cfg(any(target_os = "macos", target_os = "freebsd"))]
+pub(self) mod lsof;
+
+#[cfg(any(target_os = "macos", target_os = "freebsd"))]
+mod lsof_utils;
+
+#[cfg(target_os = "windows")]
+pub(self) mod windows;
+
 mod errors;
 pub(crate) mod shared;
 
